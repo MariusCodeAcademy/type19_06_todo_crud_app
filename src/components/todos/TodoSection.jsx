b78c0fb27b5f3541d881542eb4dff1d667a6e76c
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-17 09:54:30 +0200

    Tobj with state

--- src/components/todos/TodoSection.jsx
+++ src/components/todos/TodoSection.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
+import SingleTodo from './SingleTodo';
 
 const initTodos = [
   { id: 1, text: 'Complete task 1', completed: false },
   { id: 2, text: 'Finish task 2', completed: true },
   { id: 3, text: 'Submit task 3', completed: true },
-  { id: 4, text: 'Review task 4', completed: false },
+  { id: 4, text: 'Review task 4', completed: true },
   { id: 5, text: 'Implement task 5', completed: false },
 ];
 
@@ -12,9 +13,20 @@ export default function TodoSection() {
   // useState
   const [mainTodoArr, setMainTodoArr] = useState(initTodos);
 
+  console.table(mainTodoArr);
+
   return (
     <div>
       <h2>Todos</h2>
+      <h3>TotalDone Todos: {mainTodoArr.filter((tObj) => tObj.completed).length}</h3>
+      <ul>
+        {mainTodoArr.map((tObj) => (
+          <li key={tObj.id}>
+            <SingleTodo todoTitle={tObj.text} todoComplete={tObj.completed} />
+          </li>
+        ))}
+      </ul>
+
       <ul>
         {/* map per state ir make todos */}
         {mainTodoArr.map((tObj) => (
